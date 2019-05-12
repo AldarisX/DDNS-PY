95fa1b014872b690e2671c7d16abca79584ffb97
aldaris <z91590416@live.cn>
2019-05-13 06:50:09 +0800

    exception info

--- util/ip.py
+++ util/ip.py
@@ -4,15 +4,23 @@ from config import Config
 
 
 def get_ip():
+    ip = "-1"
     if(Config().get_use_loc_ip()):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             s.connect(('8.8.8.8', 80))
             ip = s.getsockname()[0]
-        finally:
             s.close()
+        except Exception as err:
+            print("获取IP失败")
     else:
-        hreq = requests.get("http://icanhazip.com/")
-        ip = hreq.text.replace("\n", "")
+        try:
+            hreq = requests.get("http://icanhazip.com/")
+            ip = hreq.text.replace("\n", "")
+        except Exception as err:
+            print("获取IP失败")
     print("Your IP is "+ip)
+    if(ip == "-1"):
+        return get_ip()
+
     return ip
